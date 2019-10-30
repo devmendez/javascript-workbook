e1c637e1b2037484a86a7d5183ec0576e997d85a
devmendez <devmendez@gmail.com>
2019-10-29 23:27:42 -0500

    loops

--- 04week/loops.js
+++ 04week/loops.js
@@ -13,16 +13,6 @@ const person = {
   gender: "female"
 };
 
-// print out birth years that are odd
-for (var i = 0; i < person.birthDate.length; i++) {
-  //   //check that the number is even
-  if (i % 2 == 0) {
-    continue;
-  }
-  // if get here, then i is odd
-  console.log(i + "  is an odd birth year");
-}
-
 //creating an array of persons that contains multiple objects
 const arrayOfPersons = [
   {
@@ -63,9 +53,24 @@ const arrayOfPersons = [
   }
 ];
 
+// print out birth years that are odd
+for (var i = 0; i < arrayOfPersons.length; i++) {
+  let persons = arrayOfPersons[i];
+  var dob = persons.birthDate;
+  var date = new Date(dob);
+  var year = date.getFullYear();
+  //check that the number is even
+  if (year % 2 == 0) {
+    continue;
+  }
+  // if get here, then i is odd
+  console.log(year + "  is an odd birth year");
+}
+
 //using .map over the arrayOfPersons and display their info
-const displayInfo = arrayOfPersons.map(function(display) {
-  return display.info;
+const displayInfo = arrayOfPersons.map(function(person) {
+  let info = `${person.firstName}, ${person.lastName}, ${person.birthDate}, ${person.gender}`;
+  return info;
 });
 console.log(displayInfo);
 
@@ -75,8 +80,11 @@ const malesOnly = arrayOfPersons.filter(males => males.gender === "male");
 console.log(malesOnly);
 
 // //filter out people born before 1990
-const bornBeforeNinety = arrayOfPersons.filter(
-  birthdate => arrayOfPersons.birthDate < 1990
-);
+let persons = arrayOfPersons[i];
+var dob = persons.birthDate;
+var date = new Date(dob);
+var year = date.getFullYear();
+
+const bornBeforeNinety = arrayOfPersons.filter(birthyear => year < 1990);
 
 console.log(bornBeforeNinety);
